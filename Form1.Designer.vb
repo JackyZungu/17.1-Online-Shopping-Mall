782f128638b7564f5cc5847b9fcf8a4dc02991cb
Lenovo-User <Lenovo-User@DESKTOP-Q92463H>
2023-10-06 08:59:59 +0200

    Add project files.

---
+++ Form1.Designer.vb
@@ -0,0 +1,276 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Label1 = New Label()
+        CheckBox1 = New CheckBox()
+        CheckBox2 = New CheckBox()
+        CheckBox3 = New CheckBox()
+        CheckBox4 = New CheckBox()
+        CheckBox5 = New CheckBox()
+        CheckBox6 = New CheckBox()
+        Button1 = New Button()
+        Label3 = New Label()
+        LblTotal = New Label()
+        Button2 = New Button()
+        Label2 = New Label()
+        Label4 = New Label()
+        Label5 = New Label()
+        Label6 = New Label()
+        Label7 = New Label()
+        Label8 = New Label()
+        Label9 = New Label()
+        Label10 = New Label()
+        SuspendLayout()
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.BackColor = SystemColors.ActiveCaption
+        Label1.Font = New Font("Segoe UI", 10.8F, FontStyle.Bold Or FontStyle.Italic, GraphicsUnit.Point)
+        Label1.Location = New Point(211, 19)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(278, 25)
+        Label1.TabIndex = 0
+        Label1.Text = "BLOCKCHAIN SHOPPING MALL"
+        ' 
+        ' CheckBox1
+        ' 
+        CheckBox1.AutoSize = True
+        CheckBox1.Location = New Point(169, 102)
+        CheckBox1.Name = "CheckBox1"
+        CheckBox1.Size = New Size(96, 24)
+        CheckBox1.TabIndex = 1
+        CheckBox1.Text = "iPhone 11"
+        CheckBox1.UseVisualStyleBackColor = True
+        ' 
+        ' CheckBox2
+        ' 
+        CheckBox2.AutoSize = True
+        CheckBox2.Location = New Point(169, 132)
+        CheckBox2.Name = "CheckBox2"
+        CheckBox2.Size = New Size(177, 24)
+        CheckBox2.TabIndex = 2
+        CheckBox2.Text = "Samsung Galaxy S10+"
+        CheckBox2.UseVisualStyleBackColor = True
+        ' 
+        ' CheckBox3
+        ' 
+        CheckBox3.AutoSize = True
+        CheckBox3.Location = New Point(169, 162)
+        CheckBox3.Name = "CheckBox3"
+        CheckBox3.Size = New Size(147, 24)
+        CheckBox3.TabIndex = 3
+        CheckBox3.Text = "Huawei Mate P30"
+        CheckBox3.UseVisualStyleBackColor = True
+        ' 
+        ' CheckBox4
+        ' 
+        CheckBox4.AutoSize = True
+        CheckBox4.Location = New Point(169, 192)
+        CheckBox4.Name = "CheckBox4"
+        CheckBox4.Size = New Size(130, 24)
+        CheckBox4.TabIndex = 4
+        CheckBox4.Text = "Xiaomo Redmi"
+        CheckBox4.UseVisualStyleBackColor = True
+        ' 
+        ' CheckBox5
+        ' 
+        CheckBox5.AutoSize = True
+        CheckBox5.Location = New Point(169, 232)
+        CheckBox5.Name = "CheckBox5"
+        CheckBox5.Size = New Size(98, 24)
+        CheckBox5.TabIndex = 5
+        CheckBox5.Text = "Vivo Nex3"
+        CheckBox5.UseVisualStyleBackColor = True
+        ' 
+        ' CheckBox6
+        ' 
+        CheckBox6.AutoSize = True
+        CheckBox6.Location = New Point(169, 272)
+        CheckBox6.Name = "CheckBox6"
+        CheckBox6.Size = New Size(109, 24)
+        CheckBox6.TabIndex = 6
+        CheckBox6.Text = "Oppo Neno"
+        CheckBox6.UseVisualStyleBackColor = True
+        ' 
+        ' Button1
+        ' 
+        Button1.Location = New Point(156, 326)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(94, 29)
+        Button1.TabIndex = 7
+        Button1.Text = "Calculate"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' Label3
+        ' 
+        Label3.AutoSize = True
+        Label3.Location = New Point(388, 335)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(60, 20)
+        Label3.TabIndex = 9
+        Label3.Text = "Total = "
+        ' 
+        ' LblTotal
+        ' 
+        LblTotal.AutoSize = True
+        LblTotal.BackColor = Color.Salmon
+        LblTotal.Location = New Point(454, 335)
+        LblTotal.Name = "LblTotal"
+        LblTotal.Size = New Size(0, 20)
+        LblTotal.TabIndex = 10
+        ' 
+        ' Button2
+        ' 
+        Button2.Location = New Point(275, 326)
+        Button2.Name = "Button2"
+        Button2.Size = New Size(94, 29)
+        Button2.TabIndex = 11
+        Button2.Text = "Reset"
+        Button2.UseVisualStyleBackColor = True
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.BackColor = Color.Lime
+        Label2.Location = New Point(177, 69)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(148, 20)
+        Label2.TabIndex = 12
+        Label2.Text = "Mobile Phone Model"
+        ' 
+        ' Label4
+        ' 
+        Label4.AutoSize = True
+        Label4.BackColor = Color.FromArgb(CByte(128), CByte(255), CByte(128))
+        Label4.Location = New Point(522, 69)
+        Label4.Name = "Label4"
+        Label4.Size = New Size(41, 20)
+        Label4.TabIndex = 13
+        Label4.Text = "Price"
+        ' 
+        ' Label5
+        ' 
+        Label5.AutoSize = True
+        Label5.Location = New Point(518, 106)
+        Label5.Name = "Label5"
+        Label5.Size = New Size(50, 20)
+        Label5.TabIndex = 14
+        Label5.Text = "R5000"
+        ' 
+        ' Label6
+        ' 
+        Label6.AutoSize = True
+        Label6.Location = New Point(518, 133)
+        Label6.Name = "Label6"
+        Label6.Size = New Size(50, 20)
+        Label6.TabIndex = 15
+        Label6.Text = "R4500"
+        ' 
+        ' Label7
+        ' 
+        Label7.AutoSize = True
+        Label7.Location = New Point(518, 162)
+        Label7.Name = "Label7"
+        Label7.Size = New Size(50, 20)
+        Label7.TabIndex = 16
+        Label7.Text = "R3500"
+        ' 
+        ' Label8
+        ' 
+        Label8.AutoSize = True
+        Label8.Location = New Point(518, 192)
+        Label8.Name = "Label8"
+        Label8.Size = New Size(50, 20)
+        Label8.TabIndex = 17
+        Label8.Text = "R2500"
+        ' 
+        ' Label9
+        ' 
+        Label9.AutoSize = True
+        Label9.Location = New Point(518, 233)
+        Label9.Name = "Label9"
+        Label9.Size = New Size(50, 20)
+        Label9.TabIndex = 18
+        Label9.Text = "R3000"
+        ' 
+        ' Label10
+        ' 
+        Label10.AutoSize = True
+        Label10.Location = New Point(518, 272)
+        Label10.Name = "Label10"
+        Label10.Size = New Size(50, 20)
+        Label10.TabIndex = 19
+        Label10.Text = "R3399"
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(8F, 20F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(800, 450)
+        Controls.Add(Label10)
+        Controls.Add(Label9)
+        Controls.Add(Label8)
+        Controls.Add(Label7)
+        Controls.Add(Label6)
+        Controls.Add(Label5)
+        Controls.Add(Label4)
+        Controls.Add(Label2)
+        Controls.Add(Button2)
+        Controls.Add(LblTotal)
+        Controls.Add(Label3)
+        Controls.Add(Button1)
+        Controls.Add(CheckBox6)
+        Controls.Add(CheckBox5)
+        Controls.Add(CheckBox4)
+        Controls.Add(CheckBox3)
+        Controls.Add(CheckBox2)
+        Controls.Add(CheckBox1)
+        Controls.Add(Label1)
+        Name = "Form1"
+        Text = "Online Shopping Mall"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents CheckBox1 As CheckBox
+    Friend WithEvents CheckBox2 As CheckBox
+    Friend WithEvents CheckBox3 As CheckBox
+    Friend WithEvents CheckBox4 As CheckBox
+    Friend WithEvents CheckBox5 As CheckBox
+    Friend WithEvents CheckBox6 As CheckBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Label3 As Label
+    Friend WithEvents LblTotal As Label
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Label8 As Label
+    Friend WithEvents Label9 As Label
+    Friend WithEvents Label10 As Label
+End Class
---
+++ Form1.vb
@@ -0,0 +1,43 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Const IPhone As Integer = 5000
+        Const Samsung As Integer = 4500
+        Const Huawei As Integer = 3500
+        Const Xiaomi As Integer = 2500
+        Const Vivo As Integer = 3000
+        Const Oppo As Integer = 3399
+        Dim sum As Integer
+        If CheckBox1.Checked = True Then
+            sum += IPhone
+
+        End If
+        If CheckBox2.Checked = True Then
+            sum += Samsung
+
+        End If
+        If CheckBox3.Checked = True Then
+            sum += Huawei
+        End If
+        If CheckBox4.Checked = True Then
+            sum += Xiaomi
+
+        End If
+        If CheckBox5.Checked = True Then
+            sum += Vivo
+
+        End If
+        If CheckBox6.Checked = True Then
+            sum += Oppo
+        End If
+        LblTotal.Text = sum.ToString("C")
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        CheckBox1.Checked = False
+        CheckBox2.Checked = False
+        CheckBox3.Checked = False
+        CheckBox4.Checked = False
+        CheckBox5.Checked = False
+        CheckBox6.Checked = False
+    End Sub
+End Class
